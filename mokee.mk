f32a42a917255b7cd6a256ac925fe28df5bb0d5c
martincz <martincz@mokeedev.com>
2016-11-01 17:59:42 +0800

    victara: mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -1,15 +1,15 @@
 $(call inherit-product, device/motorola/victara/full_victara.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
-PRODUCT_NAME := cm_victara
+PRODUCT_NAME := mk_victara
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
