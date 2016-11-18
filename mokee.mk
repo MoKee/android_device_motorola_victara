75df021522fa4a9cbe9b07fa6365470527214d05
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 03:36:00 -0600

    victara: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8

--- mokee.mk
+++ mokee.mk
@@ -3,9 +3,6 @@ $(call inherit-product, device/motorola/victara/full_victara.mk)
 # Inherit some common MK stuff.
 $(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
-
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
