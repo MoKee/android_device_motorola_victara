48c0c714e165ebdb0e61eaf627887e3f2ea7c8b2
Chenxi Mao <chenxi.mao@sonymobile.com>
2016-11-28 17:01:12 +0800

    BT-HFP: HFP 1.7 AG support for 8974
    Added HFP 1.7 support for 8974.
    
    Change-Id: I2bf940701d88ef0abf4a99280352721d13a71368

--- rootdir/etc/init.qcom.bt.sh
+++ rootdir/etc/init.qcom.bt.sh
@@ -154,7 +154,7 @@ config_bt ()
     "msm8974" | "msm8226" | "msm8610" )
        if [ "$btsoc" != "ath3k" ]
        then
-           setprop ro.bluetooth.hfp.ver 1.6
+           setprop ro.bluetooth.hfp.ver 1.7
            setprop ro.qualcomm.bt.hci_transport smd
            setprop qcom.bluetooth.soc smd
        fi
