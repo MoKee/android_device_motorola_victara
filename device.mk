8acb780eda19de7b311bbb0fd0512d091e17351d
Chenxi Mao <chenxi.mao2013@gmail.com>
2018-02-28 14:12:59 +0800

    victara: Tuning some parameters.

--- device.mk
+++ device.mk
@@ -82,6 +82,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Art
 PRODUCT_PROPERTY_OVERRIDES += \
