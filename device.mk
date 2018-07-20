3fde31c8292df7b77734de3eed8ac79ed2d1f2cc
Martincz Gao <martincz@mokeedev.com>
2018-07-20 22:36:03 +0800

    victara: Drop dalvik/hwui
    Change-Id: I02ef27653669879f9ed5bba1077be9eba747f607

--- device.mk
+++ device.mk
@@ -81,9 +81,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
-
 # Art
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false
